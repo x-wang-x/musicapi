0672090b05324516f1d0f568a9e23008657f3552
Fajar Xuss <fajarxuss@gmail.com>
2020-08-13 16:50:32 +0700

    added singer profile

--- route/song.js
+++ route/song.js
@@ -7,6 +7,30 @@ router.get('/id', (req, res,next) => {
    res.send('nothing')
 })
 
+router.get('/singer', (req, res,next) => {
+   const id = req.query.id
+   const s = req.query.start
+   const e = req.query.end
+   const baseUrl = `http://api.joox.com/web-fcgi-bin/web_album_singer?cmd=2&singerid=${id}&sin=${s}&ein=${e}&lang=id&country=id&callback=xk3n`
+     Axios.request({
+     url: baseUrl,
+     method: "get",
+     headers:{
+         Cookie: "wmid=142420656; user_type=1; country=id; session_key=2a5d97d05dc8fe238150184eaf3519ad;"
+     }}).then((response)=>{
+
+     	let data=response.data
+		data=data.replace('xk3n(','')
+		data=data.replace(')','')
+
+		res.setHeader('Content-Type', 'application/json');
+	    res.end(data)
+
+     }).catch(error =>{
+        res.send(error)
+    })
+})
+
 router.get('/search',(req,res,next)=>{
 	const q = req.query.query
 	const p = req.query.pag
