cf7e4cd43c283fffe678f916c6e6f6e3896cef16
Fajar Xuss <fajarxuss@gmail.com>
2020-08-13 12:51:34 +0700

    added search and fix song by id

--- route/song.js
+++ route/song.js
@@ -7,9 +7,35 @@ router.get('/id', (req, res,next) => {
    res.send('nothing')
 })
 
-router.get('/song/:query',(req,res,next)=>{
-	const query = req.params.query
-	const baseUrl = `http://api.joox.com/web-fcgi-bin/web_get_songinfo?songid=${query}&lang=id&country=id&from_type=null&channel_id=null`
+router.get('/search',(req,res,next)=>{
+	const q = req.query.query
+	const p = req.query.pag
+	const s = req.query.start
+	const e= req.query.end
+	const baseUrl= `http://api.joox.com/web-fcgi-bin/web_search?callback=xk3n&lang=id&country=id&type=0&search_input=${q}&pn=${p}&sin=${s}&ein=${e}&_=`
+	 Axios.request({
+     url: baseUrl,
+     method: "get",
+     headers:{
+         Cookie: "wmid=142420656; user_type=1; country=id; session_key=2a5d97d05dc8fe238150184eaf3519ad;"
+     }}).then((response)=>{
+
+	let data=response.data
+	data=data.replace('xk3n(','')
+	data=data.replace(')','')
+
+	res.setHeader('Content-Type', 'application/json');
+    res.end(data)
+
+	}).catch(error =>{
+        res.send(error)
+    })
+
+})
+
+router.get('/song/:id',(req,res,next)=>{
+	const song_id = req.params.id
+	const baseUrl = `http://api.joox.com/web-fcgi-bin/web_get_songinfo?songid=${song_id}&lang=id&country=id&from_type=null&channel_id=null`
     Axios.request({
      url: baseUrl,
      method: "get",
@@ -22,28 +48,11 @@ router.get('/song/:query',(req,res,next)=>{
 	let title,singer,album,release,cover,r192Url,r320Url,singer_id,msg
 	data=data.replace('MusicInfoCallback(','')
 	data=data.replace(')','')
-	const obj = JSON.parse(data)
-	msg=obj.msg
+	
+	res.setHeader('Content-Type', 'application/json');
+    res.end(data)
 
-	if (msg=='invaid songid'){
-		msg='Song ID is incorrect !'
-		details.push({msg})
-		res.json(details)
-		console.log(`incorrect song with id ${query}`)
-	}
-	else {
-	album = obj.malbum
-	title=obj.msong
-	cover=obj.imgSrc
-	singer=obj.msinger
-	release=obj.public_time
-	r192Url=obj.r192Url
-	r320Url=obj.r320Url
-	singer_id=obj.msingerid
-
-	details.push({msg,title,singer,album,cover,release,singer_id,r192Url,r320Url})
-            res.json(details)
-    }}).catch(error =>{
+    }).catch(error =>{
         res.send(error)
     })
 })
